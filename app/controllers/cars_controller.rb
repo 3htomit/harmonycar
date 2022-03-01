e86e5dd432006843c9fe61a1e960ad9d929a658f
Francois Clair Delacour <fcdelacour@gmail.com>
2022-03-01 17:30:33 +0100

    new vehicule page start

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,7 @@
 class CarsController < ApplicationController
 
+
+
   def index
     @cars_user = []
 
@@ -10,5 +12,24 @@ class CarsController < ApplicationController
 
   def show
     @car = Car.find(params[:id])
+  end
+
+
+  def new
+    @specifications = Specification.all
+
+    if params[:query].present?
+      @specifications = @specifications.where('make ILIKE ?', "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'components/cars/list', local: { specifications: @specifications }, formats: [:html] }
+    end
+    @car = Car.new
+  end
+
+  def create
+
   end
 end
