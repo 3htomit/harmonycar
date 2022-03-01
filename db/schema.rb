d0e69fc6e5c6f1b961463783c1010e9c3d0bb7c8
Francois Clair Delacour <fcdelacour@gmail.com>
2022-03-01 12:54:47 +0100

    changes horsepower to engine

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_155947) do
+ActiveRecord::Schema.define(version: 2022_03_01_115202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,13 +78,13 @@ ActiveRecord::Schema.define(version: 2022_02_28_155947) do
     t.string "model"
     t.string "fuel"
     t.integer "critair"
-    t.integer "horsepower"
     t.string "transmission"
     t.date "start_year"
     t.date "end_year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "cylinder"
+    t.string "engine"
   end
 
   create_table "users", force: :cascade do |t|
