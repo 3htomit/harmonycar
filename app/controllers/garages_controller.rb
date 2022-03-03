73a1046f0d32505da09a4294705899d5fd2c4a04
cloteau camille <camille.cloteau@gmail.com>
2022-03-03 13:07:25 +0100

    take phone number of garages

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -4,8 +4,6 @@ class GaragesController < ApplicationController
     require "open-uri"
     require "nokogiri"
 
-    # @garages_name = []
-    # @garages_address = []
     @garages = []
 
     ingredient = "Nantes-44000.html"
@@ -14,11 +12,29 @@ class GaragesController < ApplicationController
     html_file = URI.open(url).read
     html_doc = Nokogiri::HTML(html_file)
 
-    html_doc.search(".titre_fiche_garage").each do |element|
-      @garages_name = element.css('a').text
-      # @garages_address = element.css('br').text
-      @garages_mail = element.css('a').attribute("href").value
-      @garages << { name: @garages_name, mail: @garages_mail }
+    xpath_address                 = "span[@class='legende_avis']/following-sibling::br/following-sibling::text()"
+    xpath_address_when_no_reviews = "h2[@class='titre_fiche_garage']/following-sibling::br/following-sibling::text()"
+
+    html_doc.search(".fiche_garage").each do |element|
+      @garages_name = element.css(".titre_fiche_garage").css('a').text
+      @garages_mail = element.css('.titre_fiche_garage').css('a').attribute("href").value
+
+      html_file_mail = URI.open("https://www.allogarage.fr/#{@garages_mail}").read
+      html_doc_mail = Nokogiri::HTML(html_file_mail)
+
+      @phone_number = []
+      html_doc_mail.search('.icon-phone').each do |phone|
+        @phone_number << phone.xpath("following-sibling::text()").text.strip.split(",")
+      end
+      @phone = @phone_number[0].uniq[0]
+
+      begin
+        @address = element.xpath(xpath_address)[0].text.strip
+      rescue => e
+        @address = element.xpath(xpath_address_when_no_reviews)[0].text.strip
+      end
+
+      @garages << { name: @garages_name, mail: @garages_mail, address: @address, phone: @phone}
     end
   end
 
