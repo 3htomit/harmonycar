6a5c1fcee61d23300e0721540176879fe7ec0ae4
cloteau camille <camille.cloteau@gmail.com>
2022-03-04 14:28:30 +0100

    add css cards garages

--- app/javascript/controllers/display_map_controller.js
+++ app/javascript/controllers/display_map_controller.js
@@ -1,10 +1,12 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "content", "map" ]
+  static targets = [ "content", "map", "btnlist", "btnmap" ]
 
   disable() {
     this.contentTarget.classList.toggle("display-none")
     this.mapTarget.classList.toggle("hidden")
+    this.btnlistTarget.classList.toggle("display-none")
+    this.btnmapTarget.classList.toggle("display-none")
   }
 }
