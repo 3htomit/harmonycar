04df5218e70908ca352c34326521e81487b858a3
cloteau camille <camille.cloteau@gmail.com>
2022-03-07 16:25:16 +0100

    add search select garages

--- app/javascript/controllers/display_map_controller.js
+++ app/javascript/controllers/display_map_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "stimulus"
+import Rails from "@rails/ujs"
 
 export default class extends Controller {
-  static targets = [ "content", "map", "btnlist", "btnmap", "garage" ]
+  static targets = [ "content", "map", "btnlist", "btnmap"]
+
+  connect(){
+    // console.log(this.choiceTarget.children);
+    // console.log(this.serviceSearchTarget.children.query.value);
+  }
 
   disable() {
     this.contentTarget.classList.toggle("display-none")
@@ -9,4 +15,18 @@ export default class extends Controller {
     this.btnlistTarget.classList.toggle("display-none")
     this.btnmapTarget.classList.toggle("display-none")
   }
+
+  garageChoice(event) {
+    event.preventDefault();
+
+
+    console.log("hello");
+    // console.log(event)
+
+    // console.log(Rails.fire(this.element, 'submit'));
+    console.log(this.element)
+    this.element.submit();
+    // Rails.fire(this.element, 'submit');
+  }
+
 }
