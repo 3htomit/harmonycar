922ea62be7ee2c25d0557d23cf2d3ac08c7b73e4
cloteau camille <camille.cloteau@gmail.com>
2022-03-10 17:02:22 +0100

    update stars yet

--- app/javascript/controllers/display_map_controller.js
+++ app/javascript/controllers/display_map_controller.js
@@ -4,14 +4,7 @@ import Rails from "@rails/ujs"
 export default class extends Controller {
   static targets = [ "content", "map", "btnlist", "btnmap"]
 
-<<<<<<< HEAD
-=======
-  connect(){
 
-    console.log(this.btnmapTarget);
-  }
-
->>>>>>> 9d922c74ae42ddb2a1f359a5810593246cd673f6
   disable() {
     console.log(this.btnlistTarget);
     this.contentTarget.classList.toggle("display-none")
@@ -23,12 +16,6 @@ export default class extends Controller {
   garageChoice(event) {
     event.preventDefault();
 
-<<<<<<< HEAD
-
-    console.log("hello");
-=======
-    console.log(this.element)
->>>>>>> 9d922c74ae42ddb2a1f359a5810593246cd673f6
     this.element.submit();
   }
 
